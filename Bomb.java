1f0a698cad010c5cd2b226d4d784017fd5b41ef2
marin-yan <marinxyan@gmail.com>
2025-04-15 09:19:50 -0400

    move bomb

--- Bomb.java
+++ Bomb.java
@@ -5,5 +5,18 @@ public class Bomb extends Actor
     public void act()
     {
         move(-3);
+        
+        if(getX() <= 0) {
+            resetBomb();
+        }
+    }
+    
+    public void resetBomb() {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num == 0){
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
