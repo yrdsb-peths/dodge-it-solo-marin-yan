d387788e22be40a5ff4a51d3c3d41e46b44983e8
marin-yan <marinxyan@gmail.com>
2025-04-15 09:04:28 -0400

    added bomb

--- MyWorld.java
+++ MyWorld.java
@@ -6,5 +6,8 @@ public class MyWorld extends World {
         
         Hero penguin = new Hero();
         addObject(penguin, 100, 100);
+        
+        Bomb bomb = new Bomb();
+        addObject(bomb, 600, 100);
     }
 }
